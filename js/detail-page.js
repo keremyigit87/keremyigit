6cdff45e2f4ded3d616601608a7559d927a7ec80
Kerem Yiğit <keremyigit87@gmail.com>
2019-12-31 03:35:47 +0300

    features

--- js/detail-page.js
+++ js/detail-page.js
@@ -9,39 +9,21 @@ var HamburgerTrigger = document.getElementById("nav-container");
 var trlTrigger = document.getElementsByClassName("trl")[0];
 
 var body = document.getElementsByTagName("body")[0];
-openAbout();
+openDetailPage();
 
-function openAbout() {
+function openDetailPage() {
   body.classList.add("show");
-  TweenMax.from(".sign-logo-wrapper", 1, {
+  TweenMax.from(".sign-logo-wrapper", 0.3, {
     width: "0px",
     ease: Power2.easeInOut
   });
-  TweenMax.from(".ky-my", 1, {
-    opacity: 0,
-    y: 100,
-    delay: 0.5,
-    ease: Power2.easeInOut
-  });
-
-  TweenMax.from(".skill-h2", 1, {
-    x: 100,
-    delay: 0.8,
-    opacity: 0,
-    ease: Power2.easeInOut
-  });
-
-  TweenMax.staggerFrom(
-    "#about .about-container ul.firstList li",
-    1,
-    {
-      opacity: 0,
-      y: 200,
-      delay: 0.7,
+  TweenMax.staggerFrom('.anim-content',1,{
+      opacity:0,
+      y:200,
+      delay:0.3,
       ease: Power2.easeInOut
-    },
-    0.1
-  );
+  },0.1);
+
   TweenMax.from(".hamburger-menu", 1, {
     opacity: 0,
     delay: 1.2,
@@ -120,16 +102,32 @@ HamburgerTrigger.addEventListener("click", function() {
   HamburgerTrigger.classList.toggle("pushed");
 });
 
+headerToAbout.addEventListener("click", function() {
+    body.classList.remove("open-menu");
+    setTimeout(function() {
+        TweenMax.to(".page-transition-black", 1, {
+            transform: "translateX(0)",
+            ease: Power2.easeInOut,
+          });
+        TweenMax.to(".page-transition-white", 1, {
+          transform: "translateX(0)",
+          ease: Power2.easeInOut,
+          delay:0.1
+        });
+      setTimeout(function() {
+        window.location.pathname = "/hakkimda";
+      }, 1200);
+    }, 500);
+  });
 
-headerToWork.addEventListener("click", function() {
-  body.classList.remove("open-menu");
-  setTimeout(function() {
-    TweenMax.to(".page-transition-black", 1, {
+  headerToWork.addEventListener("click", function() {
+    TweenMax.to(".page-transition-white", 1, {
       transform: "translateX(0)",
       ease: Power2.easeInOut
     });
     setTimeout(function() {
       window.location.pathname = "/works";
     }, 1200);
-  }, 500);
-});
+  });
+
+  
